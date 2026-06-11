65a26323a966228a5a62a2b467556296d345f321
David Theran <davidt@sempertex.local>
2026-06-11 14:35:56 -0500

    commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,6 +4,6 @@ import { Controller, Get } from '@nestjs/common';
 export class AppController {
   @Get()
   getHello(): string {
-    return 'hola mundo esto es un test';
+    return 'hola mundo esto es un test v1';
   }
 }
